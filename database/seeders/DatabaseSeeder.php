d61fe9278c8d8f7385ce6c3f98281034979f159c
unknown <m.sawah1990@gmail.com>
2025-11-27 09:20:57 +0200

    fix factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,11 +16,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // Create admin user
-        User::factory()->create([
+        // Create admin user directly
+        User::create([
             'name' => 'Admin User',
             'email' => 'admin@pawapp.net',
-            'password' => bcrypt('password123'),
+            'password' => Hash::make('password123'),
+            'email_verified_at' => now(),
         ]);
 
         // Call other seeders
